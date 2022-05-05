0f30c37857ecd77f06d12270a89e1a27c0ba404f
Ana Xavier <78264318+ana-xavier@users.noreply.github.com>
2022-05-05 09:42:38 -0300

    Understanding methods.

---
+++ 0504/asterisks.java
@@ -0,0 +1,9 @@
+public class asterisks
+{
+    public static void asterisks(){
+        int count;
+        for(count = 1; count <= 20; count++)
+            System.out.print("*");
+        System.out.println();
+    }
+}
---
+++ 0504/exemComentario.java
@@ -0,0 +1,11 @@
+public class exemComentario
+{
+    /**
+     * Calcula o volume do cubo.
+     * @param sizeLength deve informar o lado do cubo.
+     * @return retorna o valor do volume do cubo.
+     */
+   public static double cubVolume(double sizeLength){
+        return sizeLength*sizeLength*sizeLength;
+    }
+}
